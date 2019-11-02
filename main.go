5eb95daacdb1dd0ec06690bea4dac6b4845fd9b3
lewism77 <lmarhin@gmail.com>
2019-11-02 15:26:36 +1100

    Add turret to tank

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"math"
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten"
@@ -19,12 +20,17 @@ const (
 	screenHeight = 600
 	screenScale  = 1
 	windowTitle  = "Burned Ground"
+	tankWidth    = 30
+	tankHeight   = 10
+	turretWidth  = 4
+	turretHeight = 20
 )
 
 var (
 	currentTank      *tank.Tank
 	projectileImage  *ebiten.Image
 	tankImage        *ebiten.Image
+	turretImage      *ebiten.Image
 	tanks            []*tank.Tank
 	projectiles      []*projectile.Projectile
 	currentTankIndex int
@@ -34,8 +40,11 @@ func init() {
 	projectileImage, _ = ebiten.NewImage(4, 4, ebiten.FilterDefault)
 	projectileImage.Fill(color.White)
 
-	tankImage, _ = ebiten.NewImage(15, 15, ebiten.FilterLinear)
+	tankImage, _ = ebiten.NewImage(tankWidth, tankHeight, ebiten.FilterDefault)
 	tankImage.Fill(color.White)
+
+	turretImage, _ = ebiten.NewImage(turretWidth, turretHeight, ebiten.FilterDefault)
+	turretImage.Fill(color.White)
 
 	projectiles = make([]*projectile.Projectile, 0)
 	tanks = make([]*tank.Tank, 0)
@@ -59,6 +68,10 @@ func init() {
 
 func update(screen *ebiten.Image) error {
 	keymap.Logic(&currentTank, &currentTankIndex, &tanks, &projectiles)
+
+	debugFps := fmt.Sprintf(`TPS: %0.2f
+	FPS: %0.2f`, ebiten.CurrentTPS(), ebiten.CurrentFPS())
+	ebitenutil.DebugPrint(screen, debugFps)
 
 	if ebiten.IsDrawingSkipped() {
 		return nil
@@ -89,6 +102,7 @@ func update(screen *ebiten.Image) error {
 
 	for _, tank := range tanks {
 		draw(screen, tankImage, tank.LocX, tank.LocY)
+		drawTurret(screen, turretImage, tank.LocX, tank.LocY, tank.Angle)
 
 		angleText := "Angle: " + fmt.Sprintf("%.2f", tank.Angle)
 		powerText := "Power: " + fmt.Sprintf("%.2f", tank.Power)
@@ -105,6 +119,13 @@ func update(screen *ebiten.Image) error {
 func draw(screen, brushImage *ebiten.Image, x, y int) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(x), float64(y))
+	screen.DrawImage(brushImage, op)
+}
+
+func drawTurret(screen, brushImage *ebiten.Image, x, y int, angle float64) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Rotate((angle - 90) * (math.Pi / 180))
+	op.GeoM.Translate(float64(x)+(tankWidth/2), float64(y))
 	screen.DrawImage(brushImage, op)
 }
 
